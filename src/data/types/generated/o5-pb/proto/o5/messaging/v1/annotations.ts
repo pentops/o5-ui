7ca48dbdb033fe522e0eb83632e35a2d656fc11c
Joshua Slate <josh.a.slate@gmail.com>
2024-01-29 08:31:14 -0800

    regenerate types

--- src/data/types/generated/o5-pb/proto/o5/messaging/v1/annotations.ts
+++ src/data/types/generated/o5-pb/proto/o5/messaging/v1/annotations.ts
@@ -1,12 +1,13 @@
 /**
  * DO NOT EDIT -- GENERATED AUTOMATICALLY via convert-proto-to-ts. Run `pnpm generate:types` from the project root to regenerate.
- * Built from: o5-pb/proto/o5/messaging/v1/messaging.proto */
+ * Built from: o5-pb/proto/o5/messaging/v1/annotations.proto */
 
 export interface O5MessagingV1Config {
     // start oneof "type"
     type?: {
         broadcast?: O5MessagingV1BroadcastConfig;
         unicast?: O5MessagingV1UnicastConfig;
+        request?: O5MessagingV1RequestConfig;
         reply?: O5MessagingV1ReplyConfig;
     }; // end oneof "type"
 }
@@ -16,6 +17,10 @@ export interface O5MessagingV1BroadcastConfig {
 }
 
 export interface O5MessagingV1UnicastConfig {
+    name?: string;
+}
+
+export interface O5MessagingV1RequestConfig {
     name?: string;
 }
 
---
+++ src/data/types/generated/o5-pb/proto/o5/messaging/v1/reqres.ts
@@ -0,0 +1,10 @@
+/**
+ * DO NOT EDIT -- GENERATED AUTOMATICALLY via convert-proto-to-ts. Run `pnpm generate:types` from the project root to regenerate.
+ * Built from: o5-pb/proto/o5/messaging/v1/reqres.proto */
+
+export interface O5MessagingV1RequestMetadata {
+    replyTo?: string;
+    // format: bytes
+    context?: string;
+}
+
