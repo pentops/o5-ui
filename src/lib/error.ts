1a80c05585e3cb1fa209ece20efe449728b1f083
Josh Slate <josh.a.slate@gmail.com>
2024-04-26 12:10:15 -0700

    update error messages for specific error codes

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,14 +1,31 @@
 import { useEffect } from 'react';
 import { useToast } from '@/components/ui/use-toast.ts';
+import { APIError } from '@pentops/jsonapi-request';
 
 export function useErrorHandler(error: any, title: string, message?: string) {
   const { toast } = useToast();
 
   useEffect(() => {
     if (error) {
+      let usableMessage = message || error.message;
+
+      if (error instanceof APIError) {
+        switch (error.status) {
+          case 401:
+            usableMessage = 'You are not authorized to perform this action.';
+            break;
+          case 403:
+            usableMessage = 'You are forbidden from performing this action.';
+            break;
+          case 503:
+            usableMessage = 'Service is currently unavailable. Please try again later.';
+            break;
+        }
+      }
+
       toast({
         title,
-        description: message || error.message,
+        description: usableMessage,
       });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
