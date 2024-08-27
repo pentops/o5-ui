299266f5889fcab3e9dd5acf315bee6e6cf1ab9e
Josh Slate <josh.a.slate@gmail.com>
2024-08-27 15:23:39 -0700

    use normalized entity cache

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { MutationNormalizationCache, NormalizationEntityCache, QueryNormalizationCache } from '@pentops/normalized-query-cache';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/theme-provider.tsx';
 import { router } from '@/pages/routes.tsx';
@@ -9,7 +10,13 @@ import '../app/globals.css';
 
 const defaultQueryClientOptions = { queries: { staleTime: 20000 } };
 
-export const queryClient = new QueryClient({ defaultOptions: defaultQueryClientOptions });
+const ec = new NormalizationEntityCache();
+
+export const queryClient = new QueryClient({
+  defaultOptions: defaultQueryClientOptions,
+  queryCache: new QueryNormalizationCache(ec),
+  mutationCache: new MutationNormalizationCache(ec),
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
